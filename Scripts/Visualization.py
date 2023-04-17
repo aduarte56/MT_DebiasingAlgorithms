a826578ebfa722a868207a1fd4f26d46a9d27da2
aduarte56 <aduarte56@users.noreply.github.com>
2023-04-17 18:34:41 +0200

    Testing Clusters

--- Scripts/Visualization.py
+++ Scripts/Visualization.py
@@ -100,7 +100,8 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 #plots the t-SNE visualization of neighbor clusters to chosen words
 def tsne_plot_similar_words(title, labels, embedding_clusters, word_clusters, a, filename=None):
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(14,10 ))
+    
     colors = cm.rainbow(np.linspace(0, 1, len(labels)))
     for label, embeddings, words, color in zip(labels, embedding_clusters, word_clusters, colors):
         x = embeddings[:, 0]
@@ -109,9 +110,10 @@ def tsne_plot_similar_words(title, labels, embedding_clusters, word_clusters, a,
         for i, word in enumerate(words):
             plt.annotate(word, alpha=0.5, xy=(x[i], y[i]), xytext=(5, 2),
                          textcoords='offset points', ha='right', va='bottom', size=6)
-    plt.legend(loc=4)
+    #plt.legend(loc=4)
+    plt.legend(bbox_to_anchor=(1.04, 0.5), loc="center left", borderaxespad=0)
     plt.title(title)
     plt.grid(True)
-    if filename:
-       plt.savefig(filename, format='png', dpi=150, bbox_inches='tight')
+    #if filename:
+     #  plt.savefig(filename, format='png', dpi=150, bbox_inches='tight')
     plt.show()
